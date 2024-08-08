df657b3d2cda5b67e3749ad0a09be00e72546cbd
chenpingxin <chenpingxin@makeblock.com>
2024-08-08 11:45:41 +0800

    feat: test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
-import { defineConfig } from "astro/config";
-import astroI18next from "astro-i18next";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [astroI18next()],
+	// site: 'http://localhost:5500',
+	build: {
+		// assetsPrefix: 'http://localhost:3006',
+  },
+	// Enable React to support React JSX components.
+	integrations: [react()],
 });
