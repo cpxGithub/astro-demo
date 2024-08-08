060bb98482a0827895c08b16d37094085d67095f
chenpingxin <chenpingxin@makeblock.com>
2024-08-08 11:52:38 +0800

    feat: c d ncdn

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 export default defineConfig({
 	// site: 'http://localhost:5500',
 	build: {
-		// assetsPrefix: 'http://localhost:3006',
+		assetsPrefix: 'https://stalwart-puppy-cbdd2d.netlify.app',
   },
 	// Enable React to support React JSX components.
 	integrations: [react()],
